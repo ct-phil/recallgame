b1590d29d3a58829b9c727c5ce4ab322566e31b1
phil <ogutuphil@gmail.com>
2023-07-02 20:22:21 +0300

    first commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "recallgame": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "recallgamed3e81e4a": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
